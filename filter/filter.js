6759a1a30fba64121f6918613f7e89a9da8a0835
LureCoder <316913962@qq.com>
2025-02-27 12:11:41 +0800

    fix for 5070

---
+++ filter/filter.js
@@ -0,0 +1,76 @@
+// 定义 JSON 数据
+const filterData = {
+    "品牌": ["华硕", "微星", "技嘉", "七彩虹", "影驰", "耕升", "索泰", "万丽"],
+    "显卡等级": ["旗舰", "次旗舰", "丐版"],
+    "系列": ["ROG - 玩家国度", "TUF GAMING - 电竞特工", "Prime - 大师系列", 
+        "AORUS - 超级雕", "GAMING - 魔鹰", "EAGLE - 猎鹰", "AERO - 雪鹰", "WINDFORCE - 风魔", 
+        "SHADOW - 幻影师", "INSPIRE - 硬派师", "VANGUARD - 神龙", "VENTUS - 万图师", "GAMING - 魔龙",  
+        "火神", "Ultra W OC", "战斧", 
+        "圣刃", "魔刃", "金属大师", "名人堂", "星耀", 
+        "AX电竞判客", 
+        "追风", 
+        "AMP", 
+        "星舰", "星际", "星云"
+    ]
+};
+
+// 动态生成过滤条件 HTML
+const filterContainer = document.getElementById('filter-container');
+
+for (const category in filterData) {
+    const filterGroup = document.createElement('div');
+    filterGroup.classList.add('filter-group');
+
+    const categoryLabel = document.createElement('label');
+    categoryLabel.textContent = category;
+    filterGroup.appendChild(categoryLabel);
+
+    filterData[category].forEach((option) => {
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.name = category;
+        checkbox.value = option;
+        checkbox.id = option.toLowerCase().replace(' ', '-');
+        filterGroup.appendChild(checkbox);
+
+        const optionLabel = document.createElement('label');
+        optionLabel.textContent = option;
+        optionLabel.htmlFor = option.toLowerCase().replace(' ', '-');
+        filterGroup.appendChild(optionLabel);
+    });
+
+    filterContainer.appendChild(filterGroup);
+}
+
+// 处理提交过滤条件的函数
+function submitFilters() {
+
+    $('#product-list div[class*="aic-card"]').remove();
+
+    const selectedFilters = {};
+    const filterGroups = document.querySelectorAll('.filter-group');
+    filterGroups.forEach((group) => {
+        const category = group.querySelector('label').textContent;
+        const checkboxes = group.querySelectorAll('input[type="checkbox"]:checked');
+        const values = Array.from(checkboxes).map(checkbox => checkbox.value);
+        if (values.length > 0) {
+            selectedFilters[category] = values;
+        }
+    });
+    console.log('选择的过滤条件:', selectedFilters);
+    // 这里可以添加将数据发送到服务器的代码
+
+    get5070TiVideoCards('5070Ti', selectedFilters, 'filter');
+}
+
+function resetFilters() {
+
+    $('#product-list div[class*="aic-card"]').remove();
+    
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox) => {
+        checkbox.checked = false;
+    });
+
+    get5070TiVideoCards('5070Ti', undefined, 'reset');
+}
---
+++ script.js
@@ -0,0 +1,136 @@
+
+$(document).ready(function () {
+    // 加载 JSON 文件
+    getVideoCardsByCardId('5070Ti', undefined);
+});
+
+// 获取所有的 .tooltip-container 元素
+const tooltipContainers = document.querySelectorAll('.tooltip-container');
+
+// 遍历每个 .tooltip-container 元素
+tooltipContainers.forEach((container) => {
+    const pElement = container.querySelector('p');
+    const tooltipElement = container.querySelector('.tooltip');
+
+    // 将 p 元素的文本内容赋值给 tooltip 元素
+    tooltipElement.textContent = pElement.textContent;
+});
+
+function getVideoCardsByCardId(cardId, cardFilter){
+    if('5070Ti' == cardId){
+        get5070TiVideoCards('5070Ti');
+    }
+}
+
+// {
+//     "品牌": [
+//         "华硕",
+//         "微星"
+//     ],
+//     "显卡等级": [
+//         "旗舰",
+//         "次旗舰"
+//     ]
+// }
+
+function get5070TiVideoCards(cardId, cardFilter, action){
+    const level = cardFilter ? cardFilter.显卡等级 : [];
+    const brand = cardFilter ? cardFilter.品牌 : [];
+    const series = cardFilter ? cardFilter.系列 : [];
+    $.getJSON(cardId + '.json', function (data) {
+        // 遍历 products 数组
+        $.each(data.products, function (index, product) {
+            // 获取 parameters 中的节点值
+            var parameters = product.parameters;
+            var name = product.name;
+            var isLevelUndeifned = !parameters.level;
+            var pLevelName = !parameters.level || (parameters.level.name || '');
+            var pBrandName = !parameters.品牌 || (parameters.品牌 || '');
+            var pSeriesName = !parameters.品牌显卡系列 || (parameters.品牌显卡系列 || '');
+            
+
+            //parameters.level.name
+            //level
+
+            
+            if(action != 'reset'){
+                if (level && level.length > 0 && pLevelName != '' && !level.includes(pLevelName)) {
+    
+                    if(level.includes("丐版") && isLevelUndeifned){
+    
+                    } else {
+                        console.log('level ' + pLevelName + ' not match with: ' + level);
+                        return;
+                    }
+    
+                }
+    
+                if (brand && brand.length > 0 && pBrandName != '' && !brand.includes(pBrandName)) {
+                    console.log('level ' + pBrandName + ' not match with: ' + brand);
+                    return;
+                }
+    
+                if (series && series.length > 0 && pSeriesName != '' && !series.includes(pSeriesName)) {
+                    console.log('level ' + pSeriesName + ' not match with: ' + series);
+                    return;
+                }
+            }
+
+            
+
+            var levelCode = "";
+            if((parameters.level)){
+
+
+                if(parameters.level.value == 1){
+                    levelCode = '<a href="" class="ribbon"><i class="fa fa-diamond"></i><span>'+parameters.level.name+'</span></a>';
+                } else if (parameters.level.value == 2){
+                    levelCode = '<a href="" class="ribbon"><i class="fa fa-star"></i><span>'+parameters.level.name+'</span></a>';
+                }
+                
+            }
+
+            // 生成 HTML 结构
+            var html = '<div class="col-md-6 pricing-col aic-card">' +
+                '<div class="pricing-card">' +
+                '<div class="pricing-header">' +
+                '<a href="' + (parameters.productURL || '') + '" target="_blank"><img class="card-logo" src="' + (parameters.imageURL || '') + '"/></a>' +
+                // '<h5>Standard</h5>' +
+                // '<a href="" class="ribbon">' +
+                // '<i class="fa fa-star"></i>' +
+                // '<span>Feature</span>' +
+                // '</a>' +
+                levelCode + 
+                '<div class="">' +
+                '<div class="price">' +
+                '<div class="currency"></div>' +
+                '<div class="plan"></div>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '<div class="pricing-feature">' +
+                '<li class="product-id"><a href="' + (parameters.productURL || '') + '" target="_blank"><p>' + name + '</p></a></li>' +
+                '<li><p>' + (parameters.品牌 || '') + '</p></li>' +
+                '<li><p>' + (parameters.品牌显卡系列 || '') + '</p></li>' +
+                '<li><p>' + (parameters.核心频率 || '') + '</p></li>' +
+                '<li><p>' + (parameters.显卡尺寸 || '') + '</p></li>' +
+                '<li><p>' + (parameters.供电相数 || '') + '</p></li>' +
+                '<li><p>' + (parameters.散热规模 || '') + '</p></li>' +
+                '<li><p>' + (parameters.是否有均热板 || '') + '</p></li>' +
+                '<li><p>' + (parameters.供电接口 || '') + '</p></li>' +
+                '<li><p>' + (parameters.插槽 || '') + '</p></li>' +
+                '<li><p>' + (parameters.输出接口 || '') + '</p></li>' +
+                '<li class="assets"><p>' + (parameters.配件 || '') + '</p></li>' +
+                '<li class="review"><a href="'+(parameters.review[0].reviewURL || '')+'" target="_blank" title="点我"><p>' + (parameters.review[0].tester || '') + '</p></a></li>' +
+                '<li class="others tooltip-container"><p>' + (parameters.其他 || '') + '</p><span class="tooltip">' + (parameters.其他 || '') + '</span></li>' +
+                '</div>' +
+                '</div>' +
+                '</div>';
+
+            // 将生成的 HTML 插入到页面中
+            $('#product-list').append(html);
+        });
+    }).fail(function () {
+        console.log('加载 JSON 文件失败');
+    });
+}
