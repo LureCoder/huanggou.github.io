b949fa49c34b7eb1c3c65dad99d281cc04a92011
LureCoder <316913962@qq.com>
2025-03-06 17:21:09 +0800

    add SOLID CORE to filter options; update 5070Ti dataset with detailed specifications and add new Colorful 5070Ti products

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ tooltipContainers.forEach((container) => {
 
 function getVideoCardsByCardId(cardId, cardFilter, action){
     if('5070Ti' == cardId){
-        getVideoCards(['./dataset/5070Ti.json'], cardFilter, action);
+        getVideoCards(['./dataset/asus_5070Ti_products.json', './dataset/gigabyte_5070Ti_products.json', './dataset/msi_5070Ti_products.json', './dataset/colorful_5070Ti_products.json', './dataset/others_5070Ti_products.json'], cardFilter, action);
     } else if ('5080' == cardId){
         getVideoCards(['./dataset/asus_5080_products.json', './dataset/gigabyte_5080_products.json', './dataset/msi_5080_products.json'], cardFilter, action);
     } else if ('5090' == cardId){
