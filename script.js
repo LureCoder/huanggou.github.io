62d30a3435ab49e47889a5c03feb792a9d4f534b
LureCoder <316913962@qq.com>
2025-03-09 12:12:10 +0800

    update navigation links and add RTX 5070 product details; ensure correct card IDs and improve layout for better user experience

--- script.js
+++ script.js
@@ -15,8 +15,10 @@ function getVideoCardsByCardId(cardId, cardFilter, action){
         getVideoCards(['./dataset/asus_5070Ti_products.json', './dataset/gigabyte_5070Ti_products.json', './dataset/msi_5070Ti_products.json', './dataset/colorful_5070Ti_products.json', './dataset/others_5070Ti_products.json'], cardFilter, action);
     } else if ('5080' == cardId){
         getVideoCards(['./dataset/asus_5080_products.json', './dataset/gigabyte_5080_products.json', './dataset/msi_5080_products.json', './dataset/colorful_5080_products.json'], cardFilter, action);
-    } else if ('5090' == cardId){
+    } else if ('5090D' == cardId){
         getVideoCards(['./dataset/asus_5090D_products.json', './dataset/gigabyte_5090D_products.json', './dataset/msi_5090D_products.json', './dataset/colorful_5090D_products.json'], cardFilter, action);
+    } else if ('5070' == cardId){
+        getVideoCards(['./dataset/asus_5090D_products.json', './dataset/gigabyte_5090D_products.json', './dataset/msi_5090D_products.json', './dataset/colorful_5070_products.json'], cardFilter, action);
     }
 }
 
